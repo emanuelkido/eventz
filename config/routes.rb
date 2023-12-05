a88eb3fb98a6969d97b4cddae9bf68715122be1e
emanuelkido <emanueljulio1@aol.com>
2023-12-05 16:22:30 -0300

    :dizzy: feat: add linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'events#index'
   
   get 'events', to: 'events#index'
-  get 'events/:id', to: 'events#show'
+  get 'events/:id', to: 'events#show', as: 'event'
 end

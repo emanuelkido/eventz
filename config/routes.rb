97638fd45559c4cb0ea368f8dbfc607997516d74
emanuelkido <emanueljulio1@aol.com>
2023-12-07 15:48:11 -0300

    :construction: feat: add edit button

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   
   get 'events', to: 'events#index'
   get 'events/:id', to: 'events#show', as: 'event'
+  get 'events/:id/edit', to: 'events#edit', as: 'edit_event'
+
 end
